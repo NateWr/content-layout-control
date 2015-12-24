9ed86a6f86ecc33edd6909352bd125e9d8c89293
Nate Wright <natew@notthisway.com>
2015-12-24 09:42:16 +0000

    Fix post template

--- src/components/templates/posts.php
+++ src/components/templates/posts.php
@@ -9,21 +9,22 @@
 
 <div class="clc-wrapper">
 	<?php foreach( $this->posts as $post ) : ?>
-		<?php $post_query = new WP_Query( array( 'post__in' => array( $post ) ) ); ?>
+		<?php $post_query = new WP_Query( array( 'post__in' => array( $post ), 'post_type' => 'any' ) ); ?>
 		<?php while( $post_query->have_posts() ) : $post_query->the_post(); ?>
 
-			<article class="clc-post">
+			<article>
 				<h3>
 					<a href="<?php echo esc_url( get_the_permalink() ); ?>">
 						<?php the_title(); ?>
 					</a>
 				</h3>
-				<div class="excerpt">
+				<div class="summary">
 					<?php the_excerpt(); ?>
 				</div>
 			</article>
 
 		<?php endwhile; ?>
+		<?php wp_reset_query(); ?>
 	<?php endforeach; ?>
 </div>
 <?php
